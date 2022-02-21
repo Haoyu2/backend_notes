7cf878082f4ce038037a9488861af76f40a1de85
Haoyu2 <33846887+Haoyu2@users.noreply.github.com>
2022-02-21 01:03:36 -0500

    'updates'

--- SongCache/src/test/java/SongCacheTest.java
+++ SongCache/src/test/java/SongCacheTest.java
@@ -2,7 +2,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -16,14 +20,32 @@ public class SongCacheTest {
         cache.recordSongPlays("ID-1", 1);
         cache.recordSongPlays("ID-2", 2);
         cache.recordSongPlays("ID-3", 5);
-
-//        assertThat(cache.getPlaysForSong("ID-1"), is(4));
-//        assertThat(cache.getPlaysForSong("ID-9"), is(-1));
-//
-//        assertThat(cache.getTopNSongsPlayed(2), contains("ID-3",
-//                "ID-1"));
-//        assertThat(cache.getTopNSongsPlayed(0), is(empty()));
     }
+
+
+    @Test
+    public void test() throws InterruptedException {
+        SongCache songs = new SongCacheImpl();
+
+
+        List<String> names = IntStream.range(1, 11)
+                .mapToObj((i) ->"ID-" + i)
+                .collect(Collectors.toList());
+
+        List<Callable<Optional>> tasks = new ArrayList<>();
+        for (String name : names){
+            for (int i = 0; i < 100; i++) {
+                tasks.add(()->{songs.recordSongPlays(name,10);return null;});
+            }
+        }
+        Executors.newCachedThreadPool().invokeAll(tasks);
+        System.out.println(songs);
+        boolean flag =  names.parallelStream()
+                .allMatch((name)->songs.getPlaysForSong(name) == 1000);
+        assertTrue(flag);
+        System.out.println(songs.getTopNSongsPlayed(3));
+    }
+
 
     @Test
     public void test1() {
